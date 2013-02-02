73f7430e7b906b3918535770725ee503065d613a
Jason C <j_cowan06@hotmail.com>
2013-02-02 11:37:08 -0700

    first commit

---
+++ src/web_parser_project/Web_parser_project.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web_parser_project;
+
+/**
+ *
+ * @author Jason
+ */
+public class Web_parser_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
