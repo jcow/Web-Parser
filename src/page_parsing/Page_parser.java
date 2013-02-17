1e34fcb666dca4ab27c6e3d766ba2ea950a04c64
Jason C <j_cowan06@hotmail.com>
2013-02-17 14:39:42 -0700

    comments

--- src/page_parsing/Page_parser.java
+++ src/page_parsing/Page_parser.java
@@ -8,6 +8,7 @@ import java.util.ListIterator;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import web_parser_project.libraries.Spell_checker;
 import web_parser_project.web_assets.Html_asset;
 import web_parser_project.web_assets.Web_asset;
 import web_parser_project.web_assets.Web_url;
@@ -18,18 +19,23 @@ import web_parser_project.web_assets.Web_url;
  */
 public class Page_parser {
     
-    public Page_parser(){}
+    private Html_asset current_html_asset;
+    private Spell_checker spell_checker;
+    
+    public Page_parser(){
+        spell_checker = Spell_checker.getInstance();
+    }
     
     public void parse(Web_url w_url){
         
         if(Web_asset.is_html_asset(w_url.get_web_asset())){
-            Html_asset asset = (Html_asset)w_url.get_web_asset();
-            parse_document(asset);
+            current_html_asset = (Html_asset)w_url.get_web_asset();
+            parse_document();
         }
     }
     
-    public void parse_document(Html_asset html_asset){
-        Document page_document = html_asset.get_contents();
+    public void parse_document(){
+        Document page_document = current_html_asset.get_contents();
         
         parse_body(page_document.body());
     }
@@ -39,6 +45,11 @@ public class Page_parser {
     }
     
     
+    /**
+     * Recurses a set of nodes and gets their children
+     * @param nodes
+     * @param tab 
+     */
     public void parse_nodes(Elements nodes, int tab){
         
         if(nodes.isEmpty() == false){
@@ -56,8 +67,10 @@ public class Page_parser {
             }
         }
         
-        
-        
     }
     
+    public void spell_check_node(Element node){
+        String internal_text = node.ownText();
+        current_html_asset.add_to_misspellings(spell_checker.find_spelling_errors(internal_text));
+    }
 }
