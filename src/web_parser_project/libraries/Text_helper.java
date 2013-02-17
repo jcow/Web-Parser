884c1f05b8d8067c6f186ac168a0f947498b3e62
Jason C <j_cowan06@hotmail.com>
2013-02-17 13:42:45 -0700

    split function

--- src/web_parser_project/libraries/Text_helper.java
+++ src/web_parser_project/libraries/Text_helper.java
@@ -10,9 +10,23 @@ package web_parser_project.libraries;
  */
 public class Text_helper {
     
-    public static final char[] end_of_word_punctuation_chars = {';', ',', '.', '?', '!', '\'', '"', ')', '(', ':'};
+    private static final String regex_for_text_splitting = "[ ]+";
+    private static final char[] end_of_word_punctuation_chars = {';', ',', '.', '?', '!', '\'', '"', ')', '(', ':'};
     
     public Text_helper(){}
+    
+    /**
+     * Splits a string into individual words based on a specific set of characters
+     * @return 
+     */
+    public static String[] split_text_to_individual_words(String the_string){
+        if(the_string != null){
+            return the_string.split(regex_for_text_splitting);
+        }
+        else{
+            return null;
+        }
+    }
     
     public static String strip_punctuation_from_end(String the_string){
         if(the_string.length() > 0){
